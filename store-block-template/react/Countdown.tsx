5bc35b8cfc88b62bbd1a4e0edee43acbb1beb630
Jonas <web@emd.art.br>
2021-08-22 04:09:45 -0300

    finalização

---
+++ store-block-template/react/Countdown.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-apollo';
+
+import { useCssHandles } from 'vtex.css-handles';
+import useProduct from 'vtex.product-context/useProduct';
+
+import { TimeSplit } from './typings/global';
+import { tick, getTwoDaysFromNow } from './utils/time';
+
+import productReleaseDate from './graphql/productReleaseDate.graphql';
+
+const DEFAULT_TARGET_DATE = getTwoDaysFromNow();
+
+interface CountdownProps {}
+
+const CSS_HANDLES = ['countdown'] as const
+
+
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  });
+  
+  const handles = useCssHandles(CSS_HANDLES);
+  
+  const { product } = useProduct()
+
+  const { data, loading, error } = useQuery(productReleaseDate, {
+   variables: {
+     slug: product?.linkText
+   },
+   ssr: false
+  })
+  
+  if (!product) {
+    return (
+      <div>
+        <span>There is no product context.</span>
+      </div>
+    )
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div>
+        <span>Erro!</span>
+      </div>
+    )
+  }
+
+  // console.log({data});
+
+  tick(data?.product?.releaseDate || DEFAULT_TARGET_DATE, setTime);
+
+  return (
+      <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
+  );
+}
+
+Countdown.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {}
+}
+
+export default Countdown
